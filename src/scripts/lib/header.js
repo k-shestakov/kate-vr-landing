fc8ce3462e18be8aabfe89017e82f5fdc2004740
Kostiantyn Shestakov <kostiashestakov10@gmail.com>
2024-09-26 09:42:12 +0300

    make build

--- src/scripts/lib/header.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('.header');
-
-window.addEventListener('scroll', (e) => {
-  if (pageYOffset > 0) {
-    header.classList.add('_bg');
-  } else {
-    header.classList.remove('_bg');
-  }
-})
